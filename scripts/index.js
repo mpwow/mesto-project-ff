d3510a42386b8892f1a920a7c50b2a057ceb7a7d
mpwow <mpwow@yandex.ru>
2024-11-22 19:41:22 +0300

    added first version

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,39 @@
-// @todo: Темплейт карточки
+// Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
 
-// @todo: DOM узлы
+// DOM узлы
+const cardList = document.querySelector(".places__list");
+const cardElement = cardTemplate.querySelector(".card");
 
-// @todo: Функция создания карточки
+// Функция создания карточки
+const createCard = function (card, delFunc) {
+    // Клонируем темплейт карточки
+    const cardElementClone = cardElement.cloneNode(true);
 
-// @todo: Функция удаления карточки
+    // Получаем изображение карточки и устанавливаем параметры
+    const cardElementCloneImage = cardElementClone.querySelector(".card__image");
+    cardElementCloneImage.src = card.link;
+    cardElementCloneImage.alt = `Изображение города ${card.name}`;
 
-// @todo: Вывести карточки на страницу
+    // Получаем заголовок карточки и устанавливаем текст
+    const cardElementCloneTitle = cardElementClone.querySelector(".card__title");
+    cardElementCloneTitle.textContent = card.name;
+
+    // Получаем кнопку удаления в карточке и устанавливаем коллбэк с удалением
+    const cardElementCloneDeleteBtn = cardElementClone.querySelector(".card__delete-button");
+    cardElementCloneDeleteBtn.addEventListener("click",()=> {
+        delFunc(cardElementCloneDeleteBtn, '.card')
+    })
+
+    return cardElementClone;
+}
+
+// Функция удаления карточки
+function deleteCard(deleteButton, selectorToDelete) {
+    deleteButton.closest(selectorToDelete).remove()
+}
+
+// Вывести карточки на страницу
+for (let i = 0; i < initialCards.length; i++) {
+    cardList.append(createCard(initialCards[i], deleteCard));
+}
